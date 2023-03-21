8e0231ae264e0275d2daaa78e11e4dc0341cb090
Orlando Duran <orlandoduranpy@outlook.es>
2023-03-21 16:46:38 -0600

    NodeJS 01

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 const express = require('express');
 const app = express();
+// Importar la base de datos 'pokedex.json'
+const { pokemon } = require('./pokedex.json');
 
 app.get('/', (req, res, next) => {
     res.status(200);
-    res.send('Bienvenido al servidor');
+    res.send('Bienvenido al pokedex');
 });
 
-app.get('/:name', (req, res, next) => {
-    const name = req.params.name;
+app.get('/pokemon', (req, res, next) => {
     res.status(200);
-    res.send(`Hola, ${name}`);
+    // const pokemon = req.params.pokemon;
+    // res.send(`Hola, ${pokemon}`);
+    res.send(pokemon);
 })
+
+app.get('/pokemon/:id', (req, res, next) => {
+    res.status(200);
+    res.send(pokemon[req.params.id - 1]);
+});
 
 app.listen(process.env.PORT || 3000, () => {
     console.log('Servidor corriendo en el puerto 3000');
